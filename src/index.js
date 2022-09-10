c9606ba15eca2fe55e13ee43168abfd5c87b2bf4
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-09-11 05:52:35 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -7,3 +7,21 @@ console.log(message1);
 const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
 
 console.log(message2);
+
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+
+const func1 = function (str) {
+  return str;
+};
+
+console.log(func1("func1です。"));
+
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+
+console.log(func2("func2です。"));
