30cc4a88db4450ad81cc949a8b44acb481487851
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-09-11 07:47:21 +0900

    配列のスプレッド構文のよくある間違い

--- src/index.js
+++ src/index.js
@@ -40,12 +40,64 @@
 // const message4 = `名前は${name}です。年齢は${age}歳です。`;
 // console.log(message4);
 
-const myProfile = ["じゃけえ", 28];
+// const myProfile = ["じゃけえ", 28];
 
-// const messgae5 = `私の名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`
-// console.log(messgae5);
+// // const messgae5 = `私の名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`
+// // console.log(messgae5);
 
-const [name, age] = myProfile;
+// const [name, age] = myProfile;
 
-const messgae6 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(messgae6);
+// const messgae6 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(messgae6);
+
+// デフォルト値
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん。`);
+// sayHello();
+
+/**
+ * スプレッド構文
+ */
+
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// // まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num3, num4, ...arr3] = arr2;
+
+// console.log(num3);
+// console.log(num4);
+// console.log(...arr3);
+
+// 配列のコピー、結合
+// const arr5 = [10, 20];
+// const arr6 = [30, 40];
+
+// const arr7 = [...arr5];
+// console.log(arr7);
+
+// const arr8 = [...arr5, ...arr6];
+// console.log(arr8);
+
+// 配列のよくある間違い
+
+const arr1 = [10, 20];
+
+// const arr2 = arr1;
+// arr2[0] = 100;
+// console.log(arr2);
+
+// console.log(arr1); // arr2のみ変更したつもりが、参照元のarr1も変更されてしまっている。。。。
+
+//　配列のよくある間違い（修正後）
+const arr2 = [...arr1]; //(←先ほどのconst arr2 = arr1;部分をスプレッド構文に変更。)
+
+arr2[0] = 100;
+console.log(arr2);
+
+console.log(arr1);
