8a43f84349f2d147d383c1facc00c071fe8be0d8
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-09-11 06:18:28 +0900

    オブジェクトを使用した分割代入

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
-const name = "じゃけえ";
-const age = 28;
+// const name = "じゃけえ";
+// const age = 28;
 
-const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
-console.log(message1);
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
 
-const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
 
-console.log(message2);
+// console.log(message2);
 
-// 従来の関数
-// function func1(str) {
+// // 従来の関数
+// // function func1(str) {
+// //   return str;
+// // }
+
+// const func1 = function (str) {
 //   return str;
-// }
+// };
 
-const func1 = function (str) {
-  return str;
+// console.log(func1("func1です。"));
+
+// // アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です。"));
+
+// // 処理が１行の時はreturnと{}ブラケットを省略することができる。
+// const func3 = (num1, num2, num3) => num1 + num2 + num3;
+// console.log(func3(10, 20, 30));
+
+// 分割代入
+const myProfile = {
+  name: "じゃけえ",
+  age: 28
 };
 
-console.log(func1("func1です。"));
+const message3 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+console.log(message3);
 
-// アロー関数
-const func2 = (str) => str;
-console.log(func2("func2です。"));
-
-const func3 = (num1, num2, num3) => {
-  return num1 + num2 + num3;
-};
-console.log(func3(10, 20, 30));
+const { name, age } = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
